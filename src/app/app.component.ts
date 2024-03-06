74bfe7e91cfa8ffaf61d02615c7204b8ad445c14
zzz <zinfinanty@gmail.com>
2024-03-06 16:35:56 +0800

    create angualr project

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  template: `
+    <div class="star_bg"></div>
+    <div class="star-field">
+    <div class="layer"></div>
+    <div class="layer"></div>
+    <div class="layer"></div>
+    <router-outlet />
+  `,
+  styles: [],
+})
+export class AppComponent {
+  title = 'zero_page';
+}
